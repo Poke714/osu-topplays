e0489200f75fb4041e640250ca8bc08b37cfb2bf
Unknown <sebastian714@t-online.de>
2018-02-24 15:14:14 +0100

    Filter by mods

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ var hideTouchscreen = true;
 var hideUnranked = false;
 var hideOverwritten = false;
 
+var includeEZ = false
+var includeHT = false
+var includeHD = false
+var includeHR = false
+var includeDT = false
+var includeFL = false
+var includeSO = false
+
+var excludeEZ = false
+var excludeHT = false
+var excludeHD = false
+var excludeHR = false
+var excludeDT = false
+var excludeFL = false
+var excludeSO = false
+
 function onload() {
     refresh()
     filter()
@@ -33,12 +49,23 @@ function filter() {
     }
     
     list.filter(function(item) {
-        if(hideUnranked && item.values().status != "Ranked")
-            return false;
-        else if(hideTouchscreen && item.values().touchscreen != null && item.values().touchscreen == "true")
-            return false
-        else if(hideOverwritten && item.values().overwritten != null && item.values().overwritten == "true")
-            return false
+        if(hideUnranked && item.values().status != "Ranked") return false;
+        else if(hideTouchscreen && item.values().touchscreen != null && item.values().touchscreen == "true") return false
+        else if(hideOverwritten && item.values().overwritten != null && item.values().overwritten == "true") return false
+        else if(includeEZ && !item.values().mods.includes("EZ")) return false
+        else if(includeHT && !item.values().mods.includes("HT")) return false
+        else if(includeHD && !item.values().mods.includes("HD")) return false
+        else if(includeHR && !item.values().mods.includes("HR")) return false
+        else if(includeDT && !item.values().mods.includes("DT")) return false
+        else if(includeFL && !item.values().mods.includes("FL")) return false
+        else if(includeSO && !item.values().mods.includes("SO")) return false
+        else if(excludeEZ && item.values().mods.includes("EZ")) return false
+        else if(excludeHT && item.values().mods.includes("HT")) return false
+        else if(excludeHD && item.values().mods.includes("HD")) return false
+        else if(excludeHR && item.values().mods.includes("HR")) return false
+        else if(excludeDT && item.values().mods.includes("DT")) return false
+        else if(excludeFL && item.values().mods.includes("FL")) return false
+        else if(excludeSO && item.values().mods.includes("SO")) return false
         else return true;
     });
     
@@ -62,4 +89,30 @@ function filterRanked() {
 function filterOverwritten() {
     hideOverwritten = !hideOverwritten;
     filter();
+}
+
+function include(mod) {
+    switch(mod) {
+        case "EZ": includeEZ = !includeEZ; document.getElementById("includeEZ").className = "mod " + (includeEZ ? "on" : "off"); break;
+        case "HT": includeHT = !includeHT; document.getElementById("includeHT").className = "mod " + (includeHT ? "on" : "off"); break;
+        case "HD": includeHD = !includeHD; document.getElementById("includeHD").className = "mod " + (includeHD ? "on" : "off"); break;
+        case "HR": includeHR = !includeHR; document.getElementById("includeHR").className = "mod " + (includeHR ? "on" : "off"); break;
+        case "DT": includeDT = !includeDT; document.getElementById("includeDT").className = "mod " + (includeDT ? "on" : "off"); break;
+        case "FL": includeFL = !includeFL; document.getElementById("includeFL").className = "mod " + (includeFL ? "on" : "off"); break;
+        case "SO": includeSO = !includeSO; document.getElementById("includeSO").className = "mod " + (includeSO ? "on" : "off"); break;
+    }
+    filter()
+}
+
+function exclude(mod) {
+    switch(mod) {
+        case "EZ": excludeEZ = !excludeEZ; document.getElementById("excludeEZ").className = "mod " + (excludeEZ ? "on" : "off"); break;
+        case "HT": excludeHT = !excludeHT; document.getElementById("excludeHT").className = "mod " + (excludeHT ? "on" : "off"); break;
+        case "HD": excludeHD = !excludeHD; document.getElementById("excludeHD").className = "mod " + (excludeHD ? "on" : "off"); break;
+        case "HR": excludeHR = !excludeHR; document.getElementById("excludeHR").className = "mod " + (excludeHR ? "on" : "off"); break;
+        case "DT": excludeDT = !excludeDT; document.getElementById("excludeDT").className = "mod " + (excludeDT ? "on" : "off"); break;
+        case "FL": excludeFL = !excludeFL; document.getElementById("excludeFL").className = "mod " + (excludeFL ? "on" : "off"); break;
+        case "SO": excludeSO = !excludeSO; document.getElementById("excludeSO").className = "mod " + (excludeSO ? "on" : "off"); break;
+    }
+    filter()
 }
